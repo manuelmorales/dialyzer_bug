9495567b8de514ac2e402977a49b577ecdfc83ca
Manuel Morales <manuelmorales@gmail.com>
2017-04-18 11:41:25 +0100

    Added commented option of specifying the libraries to scan

--- mix.exs
+++ mix.exs
@@ -2,12 +2,18 @@ defmodule DialyzerBug.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :dialyzer_bug,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :dialyzer_bug,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      # dialyzer: [
+      #   plt_add_deps: :transitive,
+      #   paths: ["_build/dev/lib/dialyzer_bug/ebin", "_build/dev/lib/httpoison/ebin"]
+      # ],
+    ]
   end
 
   def application do
